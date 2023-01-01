e8a6d64313a63fb265de5cda8521dd820c5ba49e
Abhinandan <abhinandang.cs.20@nitj.ac.in>
2023-01-02 00:12:33 +0530

    Start

---
+++ Company1(Microsoft)/ques1.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//Aliases
+using ll= long long;
+using lld= long double;
+using ull= unsigned long long;
+
+#define vi vector<int>
+#define pii pair<int,int>
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int,int>>
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define ff first
+#define ss second
+#define pb push_back
+#define vr(v) v.begin(),v.end()
+#define rv(v) v.end(),v.begin()
+
+// Mathematical functions
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
+ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
+ll moduloMultiplication(ll a,ll b,ll mod){ll res = 0;a %= mod;while (b){if (b & 1)res = (res + a) % mod;b >>= 1;}return res;}
+ll powermod(ll x, ll y, ll p){ll res = 1;x = x % p;if (x == 0) return 0;while (y > 0){if (y & 1)res = (res*x) % p;y = y>>1;x = (x*x) % p;}return res;}
+
+//Constants
+const lld pi= 3.141592653589793238;
+const ll INF= 1e18;
+const ll mod=1e9+7;
+
+void solve(){
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+return 0;
+}
